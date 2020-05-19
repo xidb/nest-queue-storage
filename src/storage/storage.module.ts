30f0f51298880619a70c1637c641acc363ae7732
Dmitry Boytsov <boytsov.dm@gmail.com>
2020-05-20 01:53:29 +0300

    Persist snapshot

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { StorageController } from './storage.controller';
 import { StorageService } from './storage.service';
 import { QueueModule } from '../queue/queue.module';
 
 @Module({
-  imports: [QueueModule],
+  imports: [QueueModule, ScheduleModule.forRoot()],
   controllers: [StorageController],
   providers: [StorageService],
 })
--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { QueueService } from '../queue/queue.service';
 import { RequestType } from './enums/request-type.enum';
@@ -9,11 +10,49 @@ import { IStorageResponse } from './interfaces/storage-response.interface';
 
 @Injectable()
 export class StorageService {
-  constructor(private readonly queueService: QueueService) {}
+  constructor(private readonly queueService: QueueService) {
+    this.storage = JSON.parse(
+      fs.readFileSync(StorageService.snapshotPath, 'utf8'),
+    );
+  }
+
+  private readonly logger = new Logger(StorageService.name);
 
   private storage = {};
+  private static readonly snapshotInterval = CronExpression.EVERY_MINUTE;
+  private static readonly snapshotPath = path.join(
+    __dirname,
+    '../..',
+    'storage',
+    'snapshot',
+  );
+  private static readonly logPath = path.join(
+    __dirname,
+    '../..',
+    'storage',
+    'log',
+  );
+
   public static readonly incomingQueue = 'INCOME_QUEUE';
   public static readonly outgoingQueue = 'OUTCOME_QUEUE';
+
+  @Cron(StorageService.snapshotInterval)
+  private async saveSnapshot(): Promise<void> {
+    const storageString = JSON.stringify(this.storage);
+
+    await fs.writeFile(StorageService.snapshotPath, storageString, () => {
+      return;
+    });
+    // TODO create multiple log files, on saving a snapshot delete
+    //  old one start writing in new to not lose some requests,
+    //  happening on deleting a file
+    await fs.writeFile(StorageService.logPath, '', () => {
+      return;
+    });
+
+    const size = (Buffer.byteLength(storageString) / 1024).toFixed(2);
+    return this.logger.debug(`Snapshot saved (~${size}K)`);
+  }
 
   public get(key: IStorageResponse['key']): IStorageResponse {
     return this.storage[key];
@@ -63,7 +102,7 @@ export class StorageService {
     }
 
     fs.appendFile(
-      path.join(__dirname, '../..', 'storage', 'log'),
+      StorageService.logPath,
       JSON.stringify(request) + '\n',
       () => {
         return;
